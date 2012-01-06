2e5c73c40ec77a36c193f8fdd3d13df33aa661a6
Senthil Padmanabhan <senthil.padmanabhan@yahoo.com>
2012-01-06 15:56:10 -0800

    impress powerpoint project first cut

---
+++ presentation.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8" />
+    <meta name="description" content="impress.js based presentation">
+    <meta name="author" content="Senthil Padmanabhan" />
+
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:regular,semibold,italic,italicsemibold|PT+Sans:400,700,400italic,700italic|PT+Serif:400,700,400italic,700italic" rel="stylesheet" />
+    <link href="style.css" rel="stylesheet" />	
+</head>
+<body>
+
+</body>
+</html>
