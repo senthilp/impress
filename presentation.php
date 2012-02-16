47a3287cd0ed973ae67fd24e9711d0f48a00bfbc
Senthil Padmanabhan <senthil.padmanabhan@yahoo.com>
2012-02-16 15:26:41 -0800

    Changes X Y coordinates and added new slide anti-patterns

--- presentation.php
+++ presentation.php
@@ -1,3 +1,10 @@
+<?php 
+	$OFFSET_Y = 0;
+	
+	function getY($base) {
+		return $base + $GLOBALS['OFFSET_Y'];
+	}
+?>
 <!doctype html>
 <html>
 <head>
@@ -25,18 +32,18 @@
 	        <br/>     	          
 	        <br/>     	          
 	        <p class="small">
-	        	senthil padmanabhan - eBay Motors - @senthil_hi
+	        	senthil padmanabhan | eBay Motors | @senthil_hi
 	        	<br/>
 	        	<span class="tiny footnote"><sup>*</sup> Front End Engineering</span>	   	        	    	        	 
 	        </p>
 	    </div>
-    	<div id="title" class="step" data-x="400" data-y="1900" data-z="-100" data-scale="2">
+    	<div id="title" class="step" data-x="400" data-y="<?php echo getY(2200); ?>" data-z="-100" data-scale="2">
 	        <h1>JavaScript</h1>	        
 	    </div>
-    	<div class="step" data-x="750" data-y="2150" data-z="-100" data-rotate="90" data-scale="2">
+    	<div class="step" data-x="750" data-y="<?php echo getY(2450); ?>" data-z="-100" data-rotate="90" data-scale="2">
 	        <p>is EVERYWHERE</p>	        
 	    </div>
-    	<div class="step" data-x="1500" data-y="4500" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">    		    		        
+    	<div class="step" data-x="1500" data-y="<?php echo getY(5650); ?>" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">    		    		        
 	        <p>CLI - rhino, cscript, jsc, exe</p>
 	        <p>WebService & BE Stack - Node.js</p>
 	        <p>Photoshop scripting</p>
@@ -44,7 +51,7 @@
 	        <p>Desktop app - Mac, Windows</p>
 	    </div>
 	    	  	    	  	    	  	    	  
-    	<div class="step" data-x="2500" data-y="7300" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
+    	<div class="step" data-x="3100" data-y="<?php echo getY(9750); ?>" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
 	        <p class="medium">
 	        	Linkedin mobile
 	        	<br/>
@@ -59,14 +66,14 @@
 	        	Google, MicroSoft ...
 	        </p>
 	    </div>	  	    	  	    	  	    	  
-    	<div class="step" data-x="3500" data-y="10000" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
+    	<div class="step" data-x="4700" data-y="<?php echo getY(13800); ?>" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
 	        <p class="small">insertAdjacentHTML <strong class="large">100% faster</strong> than innerHTML </p>
 	        <p class="small"><a href="http://jsperf.com/insertadjacenthtml-perf" target="_blank">http://jsperf.com/insertadjacenthtml-perf</a></p>
 	    </div>
-    	<div class="step" data-x="4500" data-y="12300" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
+    	<div class="step" data-x="6200" data-y="<?php echo getY(17500); ?>" dsata-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
 	        <p>Avoid {evil} <strong class="xlarge">eval</strong></p>
 	    </div>
-    	<div id ="eval" class="step" data-x="5400" data-y="14500" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
+    	<div id ="eval" class="step" data-x="7400" data-y="<?php echo getY(20600); ?>" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
 	        <p class="medium">If <strong>absolutely</strong> needed do this instead</p>	   
         	<pre class="xsmall">
         var execScript = function(rawScript) {
@@ -89,8 +96,8 @@
             }
         };
         	</pre>
-	    </div>
-    	<div id="gwindow" class="step" data-x="6500" data-y="17500" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
+	    </div>	    
+    	<div id="gwindow" class="step" data-x="9100" data-y="<?php echo getY(25000); ?>" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
 	        <p class="medium">Avoid global variables, if used prefix with <strong><q>"window."</q></strong></p>
 	        <pre class="small">
  $.ajax("http://ebay.com/item", function(response) {
@@ -101,11 +108,11 @@
  work since itemConfig is not available in global scope</em>
 	        </pre>
 	    </div>		    	    	  	    	  	    	  	    	  	    	    	  	    	  	    	  	    	  
-    	<div id="nodeList" class="step" data-x="7500" data-y="20200" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
+    	<div id="nodeList" class="step" data-x="10400" data-y="<?php echo getY(29100); ?>" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
 	        <p>In JQuery</p>
 	        <p class="medium">$('.box') !== document.getElementsByClassName('box')</p>
 	    </div>
-    	<div class="step" data-x="8300" data-y="22400" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
+    	<div class="step" data-x="11700" data-y="<?php echo getY(32500); ?>" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
 	        <p class="medium">Avoid <strong>try-catch-finally</strong> in performance-critical functions</p>
 	        <pre class="small">
   var oProperties = ['first','second','third',...,'nth'], 
@@ -120,7 +127,7 @@
   }</pre>  			
 	        <p class="small"><br/><a href="http://jsperf.com/try-catch-if-else" target="_blank">http://jsperf.com/try-catch-if-else</a></p>
 	    </div>	  
-    	<div id="reflow" class="step" data-x="9500" data-y="25600" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
+    	<div id="reflow" class="step" data-x="13500" data-y="<?php echo getY(37150); ?>" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
 	        <p>Keep reflows minimum</p>
 	        <p class="lalign small"> 
 	        	- Hide elements before making changes
@@ -129,8 +136,12 @@
 	        	<br/>
 	        	- Clone nodes, instead of direct modification
 	        </p>
+	    </div>	    
+   		<div class="step" data-x="14900" data-y="<?php echo getY(40700); ?>" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
+	        <p>Animations</p>
+	        <p class="medium">Use <strong>requestAnimationFrame</strong> over <strong>setInterval</strong></p>
 	    </div>	    	    	    	  	    	  	    	  	    	  	    	  	    	  	    	  
-    	<div class="step" data-x="10300" data-y="28000" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
+    	<div class="step" data-x="16100" data-y="<?php echo getY(44100); ?>" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
 	        <p>Coding Style</p> 
 	        <p class="medium">Curly braces {}	left or right position matters in JS </p>
 	        <pre class="medium">
@@ -141,26 +152,40 @@
  <strong>SILENT ERROR</strong>          <strong>Works fine</strong>
 	        </pre>
 	    </div>
-   		<div class="step" data-x="11300" data-y="31000" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
+    	<div class="step" data-x="17400" data-y="<?php echo getY(48400); ?>" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
+	        <p class="medium">Avoid <strong>Anti-Patterns</strong></p>
+	        <pre class="small">
+    <strong>// antipattern</strong>
+    if(navigator.userAgent.indexOf('MSIE') !== -1) {    
+        document.attachEvent('onclick', doSomething);
+    }
+    <strong>// better</strong>
+    if(document.attachEvent) {
+        document.attachEvent('onclick', doSomething);
+    }
+    <strong>// even better</strong>
+    if(typeof document.attacheEvent != "undefined") {
+        document.attachEvent('onclick', doSomething);
+    }
+</pre>  			
+	        <p class="small"><br/><em>Complete Reference:</em> <a href="http://jsperf.com/try-catch-if-else" target="_blank">http://shichuan.github.com/javascript-patterns/</a></p>
+	    </div> 	
+   		<div class="step" data-x="18800" data-y="<?php echo getY(52850); ?>" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
 	        <p>Use <strong>async</strong> & <strong>defer</strong> script attributes wisely!</p>
 	    </div>
-   		<div class="step" data-x="12100" data-y="33300" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
-	        <p>Animations</p>
-	        <p class="medium">Use <strong>requestAnimationFrame</strong> over <strong>setInterval</strong></p>
-	    </div>
-   		<div class="step" data-x="12900" data-y="35500" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
+   		<div class="step" data-x="19900" data-y="<?php echo getY(56150); ?>" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
 	        <p>and finally...</p>
 	    </div>
-   		<div class="step" data-x="13700" data-y="37700" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
+   		<div class="step" data-x="21000" data-y="<?php echo getY(59450); ?>" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
 	        <p class="medium">This presenation was <strong>NOT</strong> created in Powerpoint</p>
 	    </div>
-   		<div class="step" data-x="14500" data-y="39900" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
+   		<div class="step" data-x="22100" data-y="<?php echo getY(62750); ?>" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
 	        <p class="medium">But with <strong class="large">impress.js</strong> web presentation library</p>
 	       	<p class="small"><a href="http://bartaz.github.com/impress.js/#/bored" target="_blank">More details: http://bartaz.github.com/impress.js/#/bored</a></p>
 	    </div>
-   		<div class="step" data-x="15300" data-y="42100" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
+   		<div class="step" data-x="23200" data-y="<?php echo getY(66000); ?>" data-z="-100" data-rotate-x="-40" data-rotate-y="30" data-scale="2">
 	        <p>Thank You</p>
-	    </div>	    		    	    	    	    	  	    		    	    	    	    	  	    	  	  	    	    	    	    	  	    		    	    	    	    	  	    	  	    	  	    	  	    	  	    	  	    	  	    	    		    	    	    	    	  	    		    	    	    	    	  	    	  	  	    	    	    	    	  	    		    	    	    	    	  	    	  	    	  	    	  	    	  	    	  	    	  	    
+	    </div>	   	    	    	    	    	    	  	    		    	    	    	    	  	    	  	  	    	    	    	    	  	    		    	    	    	    	  	    	  	    	  	    	  	    	  	    	  	    	  	    	    		    	    	    	    	  	    		    	    	    	    	  	    	  	  	    	    	    	    	  	    		    	    	    	    	  	    	  	    	  	    	  	    	  	    	  	    	  	    
 	</div>
 	<script src="impress.js"></script>
 </body>
